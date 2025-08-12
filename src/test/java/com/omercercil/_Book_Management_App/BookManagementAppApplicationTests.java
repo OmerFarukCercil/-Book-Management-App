fece51bab867f47ef389a16410943d4355fc3764
OmerFarukCercil <cercil.omerfaruk@gmail.com>
2025-08-12 12:46:11 +0300

    First commit
    Add Spring Initializr project files

---
+++ src/test/java/com/omercercil/_Book_Management_App/BookManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omercercil._Book_Management_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
